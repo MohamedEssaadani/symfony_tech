e51ce617d138c790292d4de54a989ebee42629ad
Essaadani <essaadani80@gmail.com>
2020-05-25 01:02:10 +0000

    Professeurs

--- src/Controller/ProfesseursController.php
+++ src/Controller/ProfesseursController.php
@@ -2,14 +2,16 @@
 
 namespace App\Controller;
 
+use App\Entity\Professeur;
 use App\Repository\ProfesseurRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ProfesseursController extends AbstractController
 {
     /**
-     * @Route("/professeurs", name="professeurs")
+     * @Route("/professeurs", name="Professeurs.browse")
      */
     public function index(ProfesseurRepository $repository)
     {
@@ -20,5 +22,18 @@ class ProfesseursController extends AbstractController
             'controller_name' => 'ProfesseursController',
             'professeurs' => $professeurs
         ]);
+    }
+
+    /**
+     * @Route("/destroy-professeur/{id}", name="Professeurs.destroy")
+     */
+
+    public function destroy(EntityManagerInterface $em, int $id)
+    {
+        $professeur = $em->getRepository(Professeur::class)->find($id);
+        $em->remove($professeur);
+        $em->flush();
+
+        return $this->redirectToRoute('Professeurs.browse');
     }
 }
